bd906b020ab61192d5e2a1e5bee528f6056b38e3
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-13 19:26:16 -0500

    Add JS code for Google Maps

--- main.js
+++ main.js
@@ -21,3 +21,31 @@ function openTrustees() {
 function openContact() {
     window.location.href = "contact.html";
 }
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Steeleville Eagles
+  const position = { lat: 38.00722, lng: -89.65966 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Steeleville Eagles
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Steeleville Eagles
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Steeleville Eagles",
+  });
+}
+
+initMap();
